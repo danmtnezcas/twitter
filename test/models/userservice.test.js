f14beb376b1eac3d0b70497d7ce1a3db404daf30
danmtnezcas <danmtnezcas@outlook.com>
2022-05-05 11:05:55 -0500

    Modificando archivo userservice.test,js, corrigiendo sintaxis

--- test/models/userservice.test.js
+++ test/models/userservice.test.js
@@ -1,8 +1,8 @@
 const UserService = require('../../app/models/userservice')
 
 describe("Tests for UserService", () => {
-    TextDecoderStream("1. Create a new user using the UseService", () => {
-        const user = new UserService.create(1, "danmtnezcas", "Daniel");
+    test("1. Create a new user using the UseService", () => {
+        const user = UserService.create(1, "danmtnezcas", "Daniel");
         expect(user.username).toBe("danmtnezcas");
         expect(user.name).toBe("Daniel");
         expect(user.id).toBe(1);
