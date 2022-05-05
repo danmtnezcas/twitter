dae9fb25db7f7e34f81c8e8e025294808a034425
danmtnezcas <danmtnezcas@outlook.com>
2022-05-05 12:37:56 -0500

    Modificando archivo userservice.test.js, agregando código para sgunda prueba

--- test/models/userservice.test.js
+++ test/models/userservice.test.js
@@ -8,4 +8,13 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
     })
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "danmtnezcas", "daniel");
+        const userInfoInList = UserService.getInfo(user);
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("danmtnezcas");
+        expect(userInfoInList[2]).toBe("Daniel");
+        expect(userInfoInList[3]).toBe("Sin bio");
+    })
 })
