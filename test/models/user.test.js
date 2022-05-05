9e874063a802414b6cd54fe4bad339d2fd4bc402
danmtnezcas <danmtnezcas@outlook.com>
2022-05-05 00:33:34 -0500

    Modificando archivo user.test.js, verificando que las propiedades no sean undefine

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,7 +3,7 @@ const User = require('../../app/models/user');
 describe("Unit Tests for User class", () => {
     test("Create an User object", () => {
         // Aquí invocas el código que vas a usar en tu app
-        const user = new User(1, "danmtnezcas", "Daniel", "Bio", "dateCreated", "lastUpdated");
+        const user = new User(1, "danmtnezcas", "Daniel", "Bio");
 
         /** Aquí validas los resultados de ese código
          *  Esta es una comparacaión que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -13,7 +13,7 @@ describe("Unit Tests for User class", () => {
         expect(user.username).toBe("danmtnezcas");
         expect(user.name).toBe("Daniel");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined(); // Verifica que el valor no sea undefine .not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined();
     })
 })
