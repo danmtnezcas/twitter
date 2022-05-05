28eb932bd3fd45976ea2667cc11a2bdb0dad75db
danmtnezcas <danmtnezcas@outlook.com>
2022-05-05 13:20:49 -0500

    Modificando archivo userservice.js, agregando funcion getInfo

--- app/models/userservice.js
+++ app/models/userservice.js
@@ -4,6 +4,15 @@ class UserService {
     static create(id, username, name) {
         return new User(id, username, name, "Sin bio")
     }
+
+    static getInfo(user) {
+        var list = [];
+        list.push(user.id);
+        list.push(user.username);
+        list.push(user.name);
+        list.push(user.bio);
+        return list;
+    }
 }
 
 module.exports = UserService;
