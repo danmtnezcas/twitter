d703dffa397351fae45187a5316c7ccd42b46b07
danmtnezcas <danmtnezcas@outlook.com>
2022-05-07 03:33:22 -0500

    Modificando archivo UserService.js, mejorando código

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -6,23 +6,27 @@ class UserService {
     }
 
     static getInfo(user) {
-        var list = [];
+        /*var list = [];
         list.push(user.id);
         list.push(user.username);
         list.push(user.name);
         list.push(user.bio);
-        return list;
+        return list;*/
+        return Object.values(user);
     }
 
     static updateUsername(user, newUsername) {
-        user.username = newUsername;
+        //user.username = newUsername;
+        user.setUsername= newUsername;
     }
 
     static getAllUsernames(listUsers) {
-        var listUser = listUsers;
+        /*var listUser = listUsers;
         var allUsernames = [];
         listUser.forEach(user => allUsernames.push(user.username));
-        return allUsernames;
+        return allUsernames;*/
+        const userUnsernames = listUsers.map(user => user.username);
+        return userUnsernames;
     }      
 }
 
