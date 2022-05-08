b0726ff54c58b39662b22e26146fbb1cbd331b4d
danmtnezcas <danmtnezcas@outlook.com>
2022-05-07 20:25:14 -0500

    Modificando archivo UserView.test.js, agregando segunda prueba

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -6,4 +6,9 @@ describe("Test for UserView", () => {
         const result = UserView.createUser(payload);
         expect(result.error).toMatch(/payload no existe/);
     })
+    test("Return an error object when try to create new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"};
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    })
 })
